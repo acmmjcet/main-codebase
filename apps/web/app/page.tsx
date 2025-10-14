690c7aea6066b89579404272a506a59590fdbb46
Adnan <adnanali23189@gmail.com>
2025-10-14 08:23:30 +0530

    fix: CD pipeline

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -58,6 +58,7 @@ export default function Index() {
               rel="noreferrer"
               className="list-item-link"
             >
+              
               <svg
                 fill="none"
                 stroke="currentColor"
