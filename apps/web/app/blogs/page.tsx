b98c13d235dd285d426fdf26204f4636533c2d3d
Adnan <adnanali23189@gmail.com>
2025-11-16 12:03:41 +0530

    fix: lint error

--- apps/web/app/blogs/page.tsx
+++ apps/web/app/blogs/page.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Blogs from "@/components/blogs/Landing";
@@ -7,13 +6,6 @@ function BlogsPage() {
   return (
     <>
         <Navbar />
-        {/* <div className="py-8">
-            <Suspense fallback={<div>Loading blogs...</div>}>
-                <div className="blogs-container">
-                    <Blogs />
-                </div>
-            </Suspense>
-        </div> */}
         <Blogs/>
         <Footer />
     </>
